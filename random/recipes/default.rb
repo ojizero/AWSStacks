e149b2319181b76247495596b4bf8f1dccd37b68
ojizero <ojizero@users.noreply.github.com>
2016-12-20 18:39:41 +0200

    Update default.rb

--- random/recipes/default.rb
+++ random/recipes/default.rb
@@ -4,19 +4,21 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-package 'httpd' do 
-	action :upgrade
-end
+package 'php'
+
+package 'httpd' #do 
+	#action :upgrade
+#end
 
 service 'httpd' do
 	action [:enable, :start]
 end
 
-file '/var/www/html/index.html' do
-	action :delete
-end
+# file '/var/www/html/index.html' do
+# 	action :delete
+# end
 
-file '/var/www/html/index.php' do
+file '/var/www/html/index.html' do
 	content '<html>
 <body>
 <h1>
