f3ac1f9389c6a587c7086718cdec6e759dc31d4e
ojizero <z.acnts.816@protonmail.ch>
2016-12-20 19:25:43 +0200

    Init: Added the first deploy recipe

--- deployy/recipes/default.rb
+++ deployy/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+the_app = search(:aws_opsworks_app).first # I assume it should store the application assigned via opsworks stacks
+
+#for now create a random file to test stuff out
+file '/var/www/html/theapp.html' do
+	content "#{the_app.to_a}"
+	mode '0775'
+	owner 'apache'
+	group 'apache'
+end
+
