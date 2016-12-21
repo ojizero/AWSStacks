30cbe07556049412b51c02fc55cd115a11ef8d72
ojizero <z.acnts.816@protonmail.ch>
2016-12-21 14:55:41 +0200

    PHP -> 7.1

--- random/recipes/default.rb
+++ random/recipes/default.rb
@@ -6,6 +6,18 @@
 
 package 'php'
 
+execute 'Add repo for PHP71' do
+	command 'wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && wget http://rpms.remirepo.net/enterprise/remi-release-7.rpm && rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm'
+end
+
+execute 'Enable the repo' do
+	command 'yum install yum-utils && yum-config-manager --enable remi-php71'
+end
+
+execute 'Do the upgrade' do
+	command 'yum update -y php'
+end
+
 package 'httpd' #do 
 	#action :upgrade
 #end
@@ -18,20 +30,20 @@ file '/var/www/html/index.html' do
 	action :delete
 end
 
-# file '/var/www/html/index.php' do
-# 	content '<html>
-# <body>
-# <h1>
-# <?php
-# echo phpversion();
-# ?>
-# </h1>
-# </body>
-# </html>'
-# 	mode '0775'
-# 	owner 'apache'
-# 	group 'apache'
-# end
+file '/var/www/html/index.php' do
+	content '<html>
+<body>
+<h1>
+<?php
+echo phpversion();
+?>
+</h1>
+</body>
+</html>'
+	mode '0775'
+	owner 'apache'
+	group 'apache'
+end
 
 #template '/var/www/html/index.html' do
 #
