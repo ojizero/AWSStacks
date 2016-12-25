eec542a20d616fa56b277f65d87c48ed42697ee4
ojizero <z.acnts.816@protonmail.ch>
2016-12-25 15:44:15 +0200

    added database and url info

--- deployy/recipes/default.rb
+++ deployy/recipes/default.rb
@@ -5,6 +5,8 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 the_app = search(:aws_opsworks_app).first
+database = search(:aws_opsworks_rds_db_instance).first
+the_LB = search(:aws_opsworks_elastic_load_balancer).first
 
 file '/var/www/html/theapp.html' do
 	content "#{the_app.to_s}"
@@ -28,17 +30,19 @@ deploy '/var/www/html/' do
 		execute 'postinstall' do
 			command "chown -R www-data:www-data #{release_path} && systemctl restart apache2"
 		end
+
+		template "#{release_path}/.env" do
+			source 'env.erb'
+  			variables ({
+  		  		:the_url => the_LB['dns_name'],
+	  		  	:dbhost => database['address'],
+  			  	:dbname => the_app['environment']['namedb'],
+  			  	:dbuser => database['db_user'], # the_app['environment']['userdb'],
+	  		  	:dbpass => database['db_password'] # the_app['environment']['passdb']
+  			})
+		end
+ 
 	end
 end
 
-# template "#{release_path}/.env" do
-# 	source 'env.erb'
-# 	variables ({
-# 		:the_url => 'heyy',
-# 		:dbhost => 'linked and such',
-# 		:dbname => the_app['environment']['namedb'],
-# 		:dbuser => the_app['environment']['userdb'],
-# 		:dbpass => the_app['environment']['passdb']
-# 	})
-# end
- 
+
